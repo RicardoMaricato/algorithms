92905fc1d92ad77b89fedd3a51fd7dbf0f1e95ae
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-03-12 19:13:41 -0300

    -Sum array

--- src/main/java/array/CompareArray.java
+++ src/main/java/array/CompareArray.java
@@ -8,9 +8,30 @@ import java.util.Scanner;
 public class CompareArray {
 
     public static void main(String[] args) {
-        List<Integer> a = Arrays.asList(3, 2, 1);
-        List<Integer> b = Arrays.asList(1, 2, 3);
-        compareArray(a, b);
+        List<Integer> a = Arrays.asList(17, 28, 30);
+        List<Integer> b = Arrays.asList(99, 16, 8);
+//        compareArray(a, b);
+        compareTriples(a, b);
+    }
+
+    static List<Integer> compareTriples(List<Integer> aList, List<Integer> bList) {
+        int alicePoints = 0;
+        int bobPoints = 0;
+        List<Integer> result = new ArrayList<>();
+
+        alicePoints = ((aList.get(0) > bList.get(0) ? 1 : 0) +
+                   (aList.get(1) > bList.get(1) ? 1 : 0) +
+                   (aList.get(2) > bList.get(2) ? 1 : 0));
+
+        bobPoints = ((aList.get(0) < bList.get(0) ? 1 : 0) +
+                (aList.get(1) < bList.get(1) ? 1 : 0) +
+                (aList.get(2) < bList.get(2) ? 1 : 0));
+
+        System.out.println(alicePoints + " " + bobPoints);
+        result.add(0, alicePoints);
+        result.add(1,bobPoints);
+        return result;
+
     }
 
     static List<Integer> compareArray(List<Integer> aList, List<Integer> bList) {
--- src/main/java/array/SumArray.java
+++ src/main/java/array/SumArray.java
@@ -7,6 +7,18 @@ public class SumArray {
         for (int i = 0; i < array.length; i++) {
             soma += array[i];
         }
+
+        long[] ar = {1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
+        aVeryBigSum(ar);
+    }
+
+    static long aVeryBigSum(long[] ar) {
+        long sum = 0;
+        for (int i=0; i < ar.length; i++) {
+            sum += ar[i];
+        }
+        System.out.println(sum);
+        return sum;
     }
 
 }
