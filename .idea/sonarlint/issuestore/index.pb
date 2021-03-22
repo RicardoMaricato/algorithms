5c3985d5075162d3ff278506aa3cee30ecb53509
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-03-22 17:51:34 -0300

    -teste meli

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ U
 %src/main/java/array/CompareArray.java,f/9/f901cee00db43125c477e5aa5cb17c089cda0730
 Q
 !src/main/java/array/SumArray.java,b/9/b9f3ab177896d921883d758250bf9b222f31b30c
+L
+src/main/java/TesteMeli.java,a/1/a1b5b8ce37ebd8a8f36efa58b3c8a950dacfe4aa
--- src/main/java/TesteMeli.java
+++ src/main/java/TesteMeli.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class TesteMeli {
 
@@ -17,7 +20,33 @@ public class TesteMeli {
         }
     }
 
+    private static List<Integer> maxDig(Integer max) {
+        List<Integer> numsList = new ArrayList<>();
+        for (int i = 1000; i <= 9999; i++) {
+            String[] nums = String.valueOf(i).split("");
+            if (!Stream.of(nums).allMatch(n -> Integer.valueOf(n) <= max)
+                    || Stream.of(nums).mapToInt(Integer::valueOf).sum() != 21)
+                continue;
+            numsList.add(Integer.valueOf(Stream.of(nums).collect(Collectors.joining())));
+        }
+        return numsList;
+    }
+
+    private static List<Integer> teste(Integer max) {
+        List<Integer> numsList = new ArrayList<>();
+        IntStream.range(1000, 9999).parallel()
+                .forEach(n -> {
+                    String[] nums = String.valueOf(n).split("");
+                    if (!Stream.of(nums).allMatch(nu -> Integer.valueOf(nu) <= max) || Stream.of(nums).mapToInt(Integer::valueOf).sum() != 21)
+                        return;
+                    numsList.add(Integer.valueOf(Stream.of(nums).collect(Collectors.joining())));
+                });
+        return numsList;
+    }
+
     public static void main(String[] args) {
         fizzBuzz(15);
+        System.out.println(maxDig(6));
+        System.out.println(teste(6));
     }
 }
